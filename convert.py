681bed919874f31650e8958ebe1d74932ee2b8cd
Everton da Rosa <everton3x@gmail.com>
2022-12-02 11:33:14 -0300

    Corrigido caminhos de diretórios

--- convert.py
+++ convert.py
@@ -14,9 +14,9 @@ pm_input_base_dir_template = Template(r'Z:\Abase\ARQUIVOSPAD\${ano}\MES${mes}')
 # Local dos txt do Legislativo
 cm_input_base_dir_template = Template(r'Z:\Abase\ARQUIVOSPAD\${ano}\CAMARA\MES${mes}')
 # Destino dos arquivos
-output_base_dir = Template(r'C:\Users\Everton\OneDrive\Prefeitura\PAD\v2\${ano}-${mes}')
+output_base_dir = Template(r'C:\Users\Everton\Desktop\Prefeitura\PAD\v2\${ano}-${mes}')
 # Destino dos arquivos para funcionar como um atalho para o mês corrente
-current_base_dir = r'C:\Users\Everton\OneDrive\Prefeitura\PAD\v2\current'
+current_base_dir = r'C:\Users\Everton\Desktop\Prefeitura\PAD\v2\current'
 
 
 def main():
